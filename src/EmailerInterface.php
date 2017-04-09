a3b0253ca0506260b64f5a6b643e13318c9478f0
Programster <sdpagent@gmail.com>
2017-04-09 17:42:29 +0000

    removing useless comment.

--- src/EmailerInterface.php
+++ src/EmailerInterface.php
@@ -2,11 +2,6 @@
 
 namespace iRAP\Emailers;
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 Interface EmailerInterface
 {
     public function send($to_name, $to_email, $subject, $message, $html_format=true);
