dee12ac1448dab7d1be86971382773ed570dab65
Programster <sdpagent@gmail.com>
2017-04-09 18:02:50 +0100

    restructuring so the code is within a src directory. adding a README file which is to be filled in at another time. Adding a testing area which is to be filled later. Adding the PhpMailer class to be filled.

---
+++ src/PhpMailerEmailer.php
@@ -0,0 +1,15 @@
+<?php
+
+/* 
+ * This class wraps around PHPMailer to send an email using SMTP. You could just use PHPMailer 
+ * directly, but if you go with this, you stick to an interface that allows you to swap emailer
+ * super easily in future.
+ */
+
+class PhpMailerEmailer implements EmailerInterface
+{
+    
+}
+
+
+
